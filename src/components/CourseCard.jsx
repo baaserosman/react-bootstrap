dc98eafd0b1dbd3f7eb98489ddae7290a99ff0b9
baaserosman <baaser.osman@gmail.com>
2022-03-14 14:36:39 +0100

    project modified

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,31 +1,37 @@
-import React from "react";
-// import CardGroup from "react-bootstrap/CardGroup";
-// import { Card, Button } from "react-bootstrap/Card";
-
-import { Card, Button } from "react-bootstrap";
+import { Container, Col, Row, Card, Button } from "react-bootstrap";
 
 const CourseCard = ({ data }) => {
   return (
-    <div className="container">
-      {data.map((item) => {
-        const { name, text, id, img } = item;
-        return (
-          <Card className="card" style={{ width: "18rem" }} key={id}>
-            <Card.Img className="img" variant="top" src={img} />
-            <Card.Body>
-              <Card.Title>{name}</Card.Title>
-              <Card.Text>{text}</Card.Text>
-              <Button
-                variant="outline-warning
+    <Container className="text-center mt-5">
+      <Row className="d-flex justify-content-center gap-2">
+        {data.map((item) => {
+          const { name, text, id, img } = item;
+          return (
+            <Col
+              className="d-flex justify-content-center"
+              sm={12}
+              md={5}
+              lg={3}
+              key={id}
+            >
+              <Card className="card" style={{ width: "18rem" }}>
+                <Card.Img className="img" variant="top" src={img} />
+                <Card.Body>
+                  <Card.Title>{name}</Card.Title>
+                  <Card.Text>{text}</Card.Text>
+                  <Button
+                    variant="outline-warning
             bg-dark"
-              >
-                Get Info
-              </Button>
-            </Card.Body>
-          </Card>
-        );
-      })}
-    </div>
+                  >
+                    Get Info
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
   );
 };
 
